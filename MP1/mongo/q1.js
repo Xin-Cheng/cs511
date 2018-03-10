f3916f3ccbe8862107e2e2ef74c836c257f4239b
Xin <x_cheng@outlook.com>
2018-03-10 13:36:57 -0800

    mongo-get max average value

--- MP1/mongo/q1.js
+++ MP1/mongo/q1.js
@@ -1,6 +1,64 @@
 conn = new Mongo();
 db = conn.getDB("mydb");
-var c = db.br.aggregate(
+// var averageReview = db.br.aggregate(
+//     [
+//         {
+//           $group:
+//             {
+//                 _id: "$business_id",
+//                 avgReview: { $avg: "$stars_y" },
+//             }
+//         },
+//         { $out : "averageReviews" }
+//       ]
+// );
+// db.averageReviews.aggregate(
+//     [
+//         {
+//           $group:
+//             {
+//                 _id: null,
+//                 maxReview: { $max: "$avgReview" },
+//             }
+//         }
+//       ]
+// );
+
+
+// var c = db.br.find({
+//     _id: {
+//         $in: db.averageReviews.find(
+//             {
+//                 avgReview: { $eq: db.averageReviews.aggregate(
+//                     [
+//                         {
+//                           $group:
+//                             {
+//                                 _id: null,
+//                                 maxReview: { $max: "$avgReview" },
+//                             }
+//                         }
+//                       ]
+//                 ).maxReview}
+//             }
+//         )}
+// }, {name: 1});
+// var c = db.averageReviews.find(
+//     {
+//         avgReview: { $eq: db.averageReviews.aggregate(
+//             [
+//                 {
+//                   $group:
+//                     {
+//                         _id: null,
+//                         maxReview: { $max: "$avgReview" },
+//                     }
+//                 }
+//               ]
+//         ).maxReview}
+//     }
+// );
+var temp = db.br.aggregate(
     [
         {
           $group:
@@ -9,14 +67,22 @@ var c = db.br.aggregate(
                 avgReview: { $avg: "$stars_y" },
             }
         },
-        {
-            $project : {
-                 "name": 1 
-                }
-        }
+        { $out : "averageReviews" }
       ]
 );
-
+var c = db.averageReviews.aggregate(
+    [
+        {
+          $group:
+            {
+                _id: null,
+                maxReview: { $max: "$avgReview" },
+            }
+        }
+      ]
+).map(function(doc) {
+    return doc.maxReview;
+});
 while(c.hasNext()) 
 {
     printjson(c.next());
