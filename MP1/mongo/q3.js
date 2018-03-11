2b10a0f4e7c7a0a34c06b79cff02585ec2278ec9
Xin <x_cheng@outlook.com>
2018-03-10 19:40:06 -0800

    mongo q3

---
+++ MP1/mongo/q3.js
@@ -0,0 +1,25 @@
+conn = new Mongo();
+db = conn.getDB("mydb");
+
+var c = db.br.aggregate(
+    [
+        {
+            $group : {
+                _id : "$state",
+                state: {"$first" : "$state"},
+                avg_rating: { $avg: "$stars_y" }
+            }
+        },
+        {
+            $sort:{ "state": 1 }
+        },
+        {
+            $project : { _id: 0}
+        }
+    ]
+);
+
+while(c.hasNext()) 
+{
+    printjson(c.next());
+}
